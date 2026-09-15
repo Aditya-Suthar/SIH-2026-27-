789e6a42990e1876a1062ace1ec640aec66f0cca
Aditya Suthar <adityabishnoi576@gmail.com>
2026-09-15 23:47:49 +0530

    helllo

--- testing4_ser.py
+++ testing4_ser.py
@@ -1,2 +1,7 @@
 MODEL_ID = "sbh013/wav2vec2-ser-ravdess-optimized"
 
+import torch
+import librosa
+import numpy as np
+
+from transformers import AutoFeatureExtractor, AutoModelForAudioClassification
